d3ca5d15961986f09f8d8c2aeff0785c40e75772
Aleksandr Volkov <kodak333vas@gmail.com>
2022-09-27 19:54:13 +0300

    two

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 
 const header= document.querySelector('header');
+
+
+
+
 window.addEventListener('scroll', ()=>{
 
     const scroll = window.scrollY;
@@ -10,29 +14,55 @@ window.addEventListener('scroll', ()=>{
     }else{
         header.classList.remove('bacHeader')
     }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
+    
+
+const tabs = document.querySelectorAll(".material_tab"),
+      tabs_parent= document.querySelector('.pumps_materials_tabs'),
+      boxs =  document.querySelectorAll(".box_item");
+
+
+
+function hideBox (){
+    boxs.forEach(item =>{
+        item.style.display = "none";
+        
+    })
+    tabs.forEach(item=>{
+        item.classList.remove('tab_active');
+        
+    
+        
+        
+    })
+
+}
+
+function showBox(i=0){
+        
+        tabs[i].classList.add("tab_active");
+        boxs[i].style.display = "flex";
+       
+    
+
+
+
+}
+hideBox();
+showBox();
+
+tabs_parent.addEventListener('click', (e)  =>{
+
+    if(e.target && e.target.classList.contains('material_tab'))
+    {
+        tabs.forEach((item,i)=>{
+            if(e.target==item){
+                hideBox();
+                showBox(i);
+            }
+        })
+    }
+
+})
+});
+
