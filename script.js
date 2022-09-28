8e0b63ac465938088ee2a032356518a361d48ae7
Aleksandr Volkov <kodak333vas@gmail.com>
2022-09-28 14:53:31 +0300

    28092022

---
+++ script.js
@@ -0,0 +1,88 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+const header= document.querySelector('header');
+
+
+
+
+window.addEventListener('scroll', ()=>{
+
+    const scroll = window.scrollY;
+    if(scroll > 0){
+        header.classList.add('bacHeader')
+
+    }else{
+        header.classList.remove('bacHeader')
+    }
+});
+    
+
+const tabs = document.querySelectorAll(".material_tab"),
+      tabs_parent= document.querySelector('.pumps_materials_tabs'),
+      boxs =  document.querySelectorAll(".box_item");
+
+
+
+function hideBox (){
+    boxs.forEach(item =>{
+        item.classList.add('hide');
+        item.classList.remove('show', 'fade');
+        
+    })
+    tabs.forEach(item=>{
+        item.classList.remove('tab_active');
+        
+    
+        
+        
+    })
+
+}
+
+function showBox(i=0){
+        boxs[i].classList.remove('hide');
+        tabs[i].classList.add("tab_active");
+        boxs[i].classList.add('show', "fade");
+       
+    
+
+
+
+}
+hideBox();
+showBox();
+
+tabs.forEach(item =>{
+    item.addEventListener('click', (e)=>{
+        if(e.currentTarget && e.currentTarget.classList.contains('material_tab'))
+    {
+        tabs.forEach((item,i)=>{
+            if(e.currentTarget==item){
+                hideBox();
+                showBox(i);
+            }
+        })
+    }
+
+})
+    })
+
+/*-------------картинка  big*/
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+})
+
+    
+
